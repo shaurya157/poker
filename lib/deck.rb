b38601296fc48f3b78ced79e092f5adc9267f59b
App Academy Student <student@appacademy.io>
2016-09-21 15:24:37 -0700

    completed the deck class and the specs for it" "

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,43 @@
-class Deck
+require_relative 'card.rb'
 
+class Deck
+  attr_accessor :cards
+
+  def initialize
+    @cards = []
+    populate
+    shuffle
+  end
+
+  def populate
+    (0..3).each do |suit|
+      color = get_suit(suit)
+      (1..13).each do |num|
+        if num == 1
+          @cards << Card.new(num, color, true)
+        else
+          @cards << Card.new(num, color)
+        end
+      end
+    end
+  end
+
+  def shuffle
+    @cards = @cards.shuffle
+  end
+
+  private
+
+  def get_suit(num)
+    case num
+    when 0
+      :clubs
+    when 1
+      :hearts
+    when 2
+      :diamonds
+    when 3
+      :spades
+    end
+  end
 end
--- spec/poker_spec.rb
+++ spec/poker_spec.rb
@@ -23,3 +23,28 @@ describe Card do
     expect(ace_card.ace).to be(true)
   end
 end
+
+describe Deck do
+
+  subject(:deck) { Deck.new }
+
+  it "initializes with a full deck of 52 cards" do
+    expect(deck.cards.length).to eq(52)
+  end
+
+  it "has an array of cards that matches a standard poker deck" do
+    suits = [:hearts, :clubs, :spades, :diamonds]
+
+    suits = suits.map do |suit|
+      deck.cards.count {|card| card.suit == suit}
+    end
+
+    expect(suits).to eq([13, 13, 13, 13])
+  end
+
+  it "has a method #shuffle which shuffles the deck" do
+    tester_deck = deck.cards
+    deck.shuffle
+    expect(deck.cards).not_to eq(tester_deck)
+  end
+end
